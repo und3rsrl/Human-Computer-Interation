1aa836325055a8428b9076772296d7127c7d87e3
Alex Chiurtu <alex.chiurtu@gmail.com>
2019-03-10 22:14:39 +0200

    Add files via upload

---
+++ Snake/Snake/ArduinoInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class ArduinoInput
+    {
+        private SerialPort _serialPort;
+
+        public ArduinoInput(string portName)
+        {
+            _serialPort = new SerialPort(portName);
+            _serialPort.Open();
+        }
+
+        public SerialPort SerialPort => _serialPort;
+
+        public void Dispose()
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.Dispose();
+            }
+        }
+    }
+}
+
